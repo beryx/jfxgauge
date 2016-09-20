1b527404391d8b1bf5e49a3ea70b4b86ae8e9795
Serban Iordache <iordache@hotmail.com>
2016-09-20 22:09:44 +0200

    use gradle 3.1

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
-./gradlew --no-daemon build asciidoc
+./gradlew --no-daemon --info --stacktrace build asciidoc javadoc
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   if [ "`git ls-remote origin gh-pages`" == "" ]; then
-    ./gradlew --no-daemon publishGhPages --rerun-tasks -PghPageType=init
+    ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=init
   fi
-  ./gradlew --no-daemon publishGhPages --rerun-tasks -PghPageType=latest
-  ./gradlew --no-daemon publishGhPages --rerun-tasks -PghPageType=version
+  ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=latest
+  ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=version
 fi
